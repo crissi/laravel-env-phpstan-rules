77b0289b3b794d2105b67143edebeef056c84505
Christian <c2n@live.dk>
2020-09-06 15:04:04 +0200

    Laravel environment phpstan rules

---
+++ src/Rules/NoEnvOutsideConfigDirRule.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types = 1);
+
+namespace Crissi\LaravelEnvPhpstanRules;
+
+use PHPStan\Analyser\Scope;
+use Illuminate\Support\Str;
+use PhpParser\Node\Name;
+
+/**
+ * @implements \PHPStan\Rules\Rule<\PhpParser\Node\Expr\FuncCall>
+ */
+class NoEnvOutsideConfigDirRule implements \PHPStan\Rules\Rule
+{
+
+    private $configPath;
+	public function __construct()
+	{
+        $this->configPath = getcwd() . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR;
+	}
+
+	public function getNodeType(): string
+	{
+		return \PhpParser\Node\Expr\FuncCall::class;
+	}
+
+	public function processNode(
+		\PhpParser\Node $node,
+		Scope $scope
+	): array
+	{
+        $isEnvCall = $node->name instanceof Name && $node->name->getLast() === 'env';
+
+        if (!$isEnvCall) {
+            return [];
+        }
+
+        if (Str::startsWith($scope->getFile(), $this->configPath)) {
+            return [];
+        }
+
+		return [
+            'no env() outside config directory is allowed. Variables will not work when configuration is cached'
+        ];
+	}
+
+}
