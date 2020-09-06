7594e27971fa682952f77c5c56b64d40ab121875
Christian <c2n@live.dk>
2020-09-06 15:36:30 +0200

    fixes

--- src/Rules/NoEnvOutsideConfigDirRule.php
+++ src/Rules/NoEnvOutsideConfigDirRule.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-namespace Crissi\LaravelEnvPhpstanRules;
+namespace Crissi\LaravelEnvPhpstanRules\Rules;
 
 use PHPStan\Analyser\Scope;
 use Illuminate\Support\Str;
